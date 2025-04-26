8a7edb026874b490be967100d4ab1a3fd861e113
zhangguoli2008 <455613306@qq.com>
2025-04-26 22:03:47 +1000

    Create utils.hpp

---
+++ utils.hpp
@@ -0,0 +1,11 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <string>
+#include <vector>
+
+namespace Utils {
+std::vector<std::string> split(const std::string& text, char delimiter);
+}
+
+#endif
